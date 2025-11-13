549f64b4d1112ab6dced588188b7a6b829329e36
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2025-11-13 21:59:30 +0100

    Feature: Added the problem statement in main method.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        /**
+         * Given an array of strings strs, group the anagrams together. You can return
+         * the answer in any order.
+         * 
+         * Input: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+         * Output: [["bat"], ["nat","tan"], ["ate", "eat", "tea"]]
+         */
         System.out.println("Hello, World!");
     }
 }
